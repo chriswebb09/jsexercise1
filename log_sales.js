4b92649e0abea849c8de131158b23cf57420590c
christopher webb <chris.webb5249@gmail.com>
2022-02-12 23:51:31 -0500

    update

--- log_sales.js
+++ log_sales.js
@@ -17,6 +17,7 @@ var used_clients = [];
 const user_name = "Michael G. Scott";
 const url_element = "https://icones.pro/wp-content/uploads/2021/08/icone-x-noir.png";
 
+
 function show() {
     var listHeader = document.getElementById("sale-list-header");
     if (listHeader.style.display === "none") {
@@ -45,6 +46,10 @@ function addSaleItem(client, reams) {
     $('#sale-list').append("<div class='sale-item row'><div class='entry-text col-sm-3 text-left'>" + user_name + "</div><div class='entry-text col-sm-4 text-left'>" + client + "</div><div class='entry-text col-sm-1'>" + reams + "</div><div class='col-sm-2'></div><div class='col-sm-1'><button type='button' class='delete-button btn btn-warning'><img src='" + url_element + "' width='20'/></button></div></div>");
 }
 
+function isEmpty(str) {
+    return (!str || str.length === 0 );
+}
+
 $(document).ready(function() {
     hide();
 
@@ -55,9 +60,31 @@ $(document).ready(function() {
     $(document).on("click", "#submit-button", function() {
         var client = $('#client-text-form').val();
         var reams = $('#reams-text-form').val();
-        clients = clients.filter(item => item !== client);
-        removeClientAndUpdateList(client);
+        
+        if (isEmpty(client)) {
+            alert("You must enter a client name.");
+            document.getElementById("client-text-form").focus();
+            return;
+        }
+        if (isEmpty(reams)) {
+            alert("You must enter a value");
+            document.getElementById("reams-text-form").focus();
+            return;
+        }
+        if (isNaN(parseInt(reams))) {
+            alert("Entry must be a number");
+            document.getElementById("reams-text-form").focus();
+            return;
+        }
+
+        if (!clients.includes(client)) {
+            clients.push(client);
+            $('#client-text-form').autocomplete({
+                source: clients
+            });
+        }
         show();
+        
         addSaleItem(client, reams);
         clearEntry();
     });
@@ -71,6 +98,10 @@ $(document).ready(function() {
             source: clients
         });
         parent.remove();
+        var emptyList = $("#sale-list").html() === "";
+        if (emptyList) {
+            hide();
+        }
     });
 });
 
